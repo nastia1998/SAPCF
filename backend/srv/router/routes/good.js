025b04603aacecd2de5088fd5782c885fd7e3a08
Anastasiya Leshchanka <a.leshchanka@LeverX.local>
2020-01-05 14:43:33 +0300

    add basic rest api for goods and orders routes

--- backend/srv/router/routes/good.js
+++ backend/srv/router/routes/good.js
@@ -5,9 +5,8 @@ const express = require("express");
 const dbClass = require(global.__base + "utils/dbClass");
 
 
-function _prepareObject(oUser, req) {
-    oUser.changedBy = "DebugUser";
-    return oUser;
+function _prepareObject(oGood, req) {
+    return oGood;
 }
 
 module.exports = () => {
@@ -20,10 +19,78 @@ module.exports = () => {
         tracer.entering("/goods", req, res);
 
         try {
-            tracer.exiting("/goods", "Get goods");
-            res.type("application/json").status(201).send(JSON.stringify({text: "Get Goods"}));
+            const db = new dbClass(req.db);
+            const oGood = _prepareObject(req.body, req);
+            const sSql = "SELECT * FROM \"GOOD\"";
+            oGood.goods = await db.executeUpdate(sSql);
+            tracer.exiting("/goods", "goods GET works");
+            res.type("application/json").status(200).send(JSON.stringify(oGood));
         } catch (e) {
             tracer.catching("/goods", e);
+            next(e);
+        }
+    });
+
+    app.post("/", async (req, res, next) => {
+
+        const logger = req.loggingContext.getLogger("/Application");
+        logger.info('goods post request');
+        let tracer = req.loggingContext.getTracer(__filename);
+        tracer.entering("/goods", req, res);
+
+        try {
+            const db = new dbClass(req.db);
+            const oGood = _prepareObject(req.body, req);
+            oGood.goid = await db.getNextval("goid");
+            const sSql = "INSERT INTO \"GOOD\" VALUES(?,?,?)";
+            const aValues = [ oGood.name, oGood.price, oGood.goid ];
+            await db.executeUpdate(sSql, aValues);
+            tracer.exiting("/goods", "goods POST works");
+
+            res.type("application/json").status(201).send(JSON.stringify(oGood));
+        } catch (e) {
+            next(e);
+        }
+    });
+
+    app.put("/:goid", async (req, res, next) => {
+        const logger = req.loggingContext.getLogger("/Application");
+        logger.info('goods put request');
+        let tracer = req.loggingContext.getTracer(__filename);
+        tracer.entering("/goods", req, res);
+
+        try {
+            const db = new dbClass(req.db);
+            const goid = req.params.goid;
+            const oGood = _prepareObject(req.body, req);
+            const sSql = "UPDATE \"GOOD\" SET \"NAME\" = ?, \"PRICE\" = ? " +
+                "WHERE \"GOID\" = ? ";
+            const aValues = [ oGood.name, oGood.price, goid ];
+            await db.executeUpdate(sSql, aValues);
+            tracer.exiting("/goods", "goods PUT works");
+
+            res.type("application/json").status(200).send(JSON.stringify(oGood));
+        } catch (e) {
+            next(e);
+        }
+    });
+
+    app.delete("/:goid", async (req, res, next) => {
+        const logger = req.loggingContext.getLogger("/Application");
+        logger.info('goods delete request');
+        let tracer = req.loggingContext.getTracer(__filename);
+        tracer.entering("/goods", req, res);
+
+        try {
+            const db = new dbClass(req.db);
+            const goid = req.params.goid;
+            const sSql = "DELETE FROM \"GOOD\" WHERE \"GOID\" = ? ";
+            const aValues = [ goid ];
+            await db.executeUpdate(sSql, aValues);
+            tracer.exiting("/goods", "goods DELETE works");
+
+            res.type("application/json").status(200).send();
+        } catch (e) {
             next(e);
         }
     });
--- backend/srv/router/routes/order.js
+++ backend/srv/router/routes/order.js
@@ -5,9 +5,8 @@ const express = require("express");
 const dbClass = require(global.__base + "utils/dbClass");
 
 
-function _prepareObject(oUser, req) {
-    oUser.changedBy = "DebugUser";
-    return oUser;
+function _prepareObject(oOrder, req) {
+    return oOrder;
 }
 
 module.exports = () => {
@@ -20,10 +19,78 @@ module.exports = () => {
         tracer.entering("/orders", req, res);
 
         try {
-            tracer.exiting("/orders", "Get orders");
-            res.type("application/json").status(201).send(JSON.stringify({text: "Get orders"}));
+            const db = new dbClass(req.db);
+            const oOrder = _prepareObject(req.body, req);
+            const sSql = "SELECT * FROM \"ORDER_ST\"";
+            oOrder.orders = await db.executeUpdate(sSql);
+            tracer.exiting("/orders", "orders GET works");
+            res.type("application/json").status(200).send(JSON.stringify(oOrder));
         } catch (e) {
             tracer.catching("/orders", e);
+            next(e);
+        }
+    });
+
+    app.post("/", async (req, res, next) => {
+
+        const logger = req.loggingContext.getLogger("/Application");
+        logger.info('orders post request');
+        let tracer = req.loggingContext.getTracer(__filename);
+        tracer.entering("/orders", req, res);
+
+        try {
+            const db = new dbClass(req.db);
+            const oOrder = _prepareObject(req.body, req);
+            oOrder.orid = await db.getNextval("orid");
+            const sSql = "INSERT INTO \"ORDER_ST\" VALUES(?,?,?,?)";
+            const aValues = [ oOrder.orid, oOrder.orderdate, oOrder.goid, oOrder.cuid ];
+            await db.executeUpdate(sSql, aValues);
+            tracer.exiting("/orders", "orders POST works");
+
+            res.type("application/json").status(201).send(JSON.stringify(oOrder));
+        } catch (e) {
+            next(e);
+        }
+    });
+
+    app.put("/:orid", async (req, res, next) => {
+        const logger = req.loggingContext.getLogger("/Application");
+        logger.info('orders put request');
+        let tracer = req.loggingContext.getTracer(__filename);
+        tracer.entering("/orders", req, res);
+
+        try {
+            const db = new dbClass(req.db);
+            const orid = req.params.orid;
+            const oOrder = _prepareObject(req.body, req);
+            const sSql = "UPDATE \"ORDER_ST\" SET \"ORDERDATE\" = ?, \"GOID\" = ?, \"CUID\" = ? " +
+                "WHERE \"ORID\" = ? ";
+            const aValues = [ oOrder.orderdate, oOrder.goid, oOrder.cuid, orid ];
+            await db.executeUpdate(sSql, aValues);
+            tracer.exiting("/orders", "orders PUT works");
+
+            res.type("application/json").status(200).send(JSON.stringify(oOrder));
+        } catch (e) {
+            next(e);
+        }
+    });
+
+    app.delete("/:orid", async (req, res, next) => {
+        const logger = req.loggingContext.getLogger("/Application");
+        logger.info('orders delete request');
+        let tracer = req.loggingContext.getTracer(__filename);
+        tracer.entering("/orders", req, res);
+
+        try {
+            const db = new dbClass(req.db);
+            const orid = req.params.orid;
+            const sSql = "DELETE FROM \"ORDER_ST\" WHERE \"ORID\" = ? ";
+            const aValues = [ orid ];
+            await db.executeUpdate(sSql, aValues);
+            tracer.exiting("/orders", "orders DELETE works");
+
+            res.type("application/json").status(200).send();
+        } catch (e) {
             next(e);
         }
     });
